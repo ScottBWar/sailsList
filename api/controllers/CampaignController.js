1d0644acda038b88ce51438759b6326bef3851ab
Scott Warner <scottbwarner@gmail.com>
2015-10-30 16:54:07 -0400

    started sails app with basic model associations and postgres DB

---
+++ api/controllers/CampaignController.js
@@ -0,0 +1,11 @@
+/**
+ * CampaignController
+ *
+ * @description :: Server-side logic for managing campaigns
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,32 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	name:{
+  		type:'string'
+  	},
+
+  	password:{
+  		type:'string',
+  		defaultsTo:'password'
+  	},
+
+  	characters:{
+  		collection: 'Character',
+  		defaultsTo:[{'name':'chumbo','class':'fighter'}]
+  	},
+
+  	campaigns:{
+  		collection: 'Campaign',
+  		defaultsTo:{'title':'Adams Wrath'}
+  	}
+
+  }
+};
+
